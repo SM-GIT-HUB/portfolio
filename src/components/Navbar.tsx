a98a2d2cb0687dd4a41dcb47a4bc7afbf33f1d2f
Soumik Majumder <soumikcode02@gmail.com>
2025-01-29 19:09:37 +0530

    portfolio website project setup. background and blackhole video

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image"
+
+function Navbar() {
+  return (
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+        <a href="#about me" className="h-auto w-auto flex flex-row items-center">
+          <Image src={'/navlogo.svg'} alt="logo" width={65} height={65} className="cursor-pointer animate-sspin" />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
